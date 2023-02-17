99836ecd1ceaf108a932e24412bf57d6d4fec836
Paras Gediya <mrparas103@gmail.com>
2023-02-17 10:45:42 +0530

    add validateion in auth strategy validate method

--- src/auth/jwt-auth-passport/jwt-auth.strategy.ts
+++ src/auth/jwt-auth-passport/jwt-auth.strategy.ts
@@ -3,11 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { plainToClass } from 'class-transformer';
 import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+import PrismaService from 'src/prisma/prisma.service';
 import JwtPayload from '../dto/jwt.payload.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class JwtAuthStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly config: ConfigService) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly prismaService: PrismaService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
@@ -16,6 +21,13 @@ export class JwtAuthStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    const { id } = payload;
+    const foundUser = await this.prismaService.user.findFirst({
+      where: { id },
+    });
+    if (!foundUser) {
+      throw new NotFoundException('User not found!');
+    }
     return plainToClass(JwtPayload, payload, {
       excludeExtraneousValues: true,
     });
